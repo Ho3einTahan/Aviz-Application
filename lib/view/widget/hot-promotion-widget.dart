b5fdcb29fe396846a24bc97a2243da69fed14ec6
MaxBased <hoseintahan84@gmail.com>
2024-03-24 14:39:32 +0330

    update  hot-promotion-widget.dart

--- lib/view/widget/hot-promotion-widget.dart
+++ lib/view/widget/hot-promotion-widget.dart
@@ -5,9 +5,10 @@ Widget HotPromotionWidget() {
   return Container(
     width: 224,
     padding: const EdgeInsets.all(16),
-    margin: const EdgeInsets.symmetric(horizontal: 14, vertical: 26),
-    decoration: BoxDecoration(
-        color: Colors.white, borderRadius: BorderRadius.circular(4)),
+    margin: const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+    decoration: BoxDecoration(boxShadow: [
+      BoxShadow(color: Colors.black.withOpacity(0.2), blurRadius: 10),
+    ], color: Colors.white, borderRadius: BorderRadius.circular(4)),
     child: Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -16,9 +17,15 @@ Widget HotPromotionWidget() {
             borderRadius: BorderRadius.circular(4),
             child: Image.asset('images/home1.png')),
         Text('ویلا ۵۰۰ متری زیر قیمت',
-            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700)),
+            style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w700,
+                overflow: TextOverflow.ellipsis)),
         Text('ویو عالی، سند تک برگ، سال ساخت ۱۴۰۲، تحویل فوری',
-            style: TextStyle(fontSize: 12, color: Colors.grey)),
+            style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+                overflow: TextOverflow.ellipsis)),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
