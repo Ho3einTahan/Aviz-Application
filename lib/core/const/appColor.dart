9b960c152bf3796dc79226af594d1397c28174f1
MaxBased <hoseintahan84@gmail.com>
2024-03-24 14:38:36 +0330

    update  appColor.dart

--- lib/core/const/appColor.dart
+++ lib/core/const/appColor.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 class AppColor{
 
   static Color primary=const Color(0xffE60023);
-  static Color grayFieldColor=Color(0xffF9FAFB);
+  static Color greyFieldColor=const Color(0xffF9FAFB);
+  static Color greyText=const Color(0xffD0D5DD);
 
 }
